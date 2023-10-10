9cc738f5e3e6dda573e9a56ca16b606f88957ef4
nazwakamila <nazwakamila111@gmail.com>
2023-10-10 11:39:34 +0700

    first commit

--- latihan.js
+++ latihan.js
@@ -1,20 +1,47 @@
+// Program untuk memeriksa apakah suatu angka adalah bilangan prima menggunakan loop FOR
+function cekBilanganPrimaFor(angka) {
+    if (angka <= 1) {
+        return false;
+    }
+    for (var i = 2; i <= Math.sqrt(angka); i++) {
+        if (angka % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 console.log("Menggunakan loop FOR:");
-for (var i = 1; i <= 10; i++) {
-    if (i % 2 === 0) {
-        console.log(i + " adalah angka genap.");
+for (var angka = 1; angka <= 20; angka++) {
+    if (cekBilanganPrimaFor(angka)) {
+        console.log(angka + " adalah bilangan prima.");
     } else {
-        console.log(i + " adalah angka ganjil.");
+        console.log(angka + " bukan bilangan prima.");
     }
 }
 
+// Program untuk memeriksa apakah suatu angka adalah bilangan prima menggunakan loop WHILE
+function cekBilanganPrimaWhile(angka) {
+    if (angka <= 1) {
+        return false;
+    }
+    var pembagi = 2;
+    while (pembagi <= Math.sqrt(angka)) {
+        if (angka % pembagi === 0) {
+            return false;
+        }
+        pembagi++;
+    }
+    return true;
+}
 
 console.log("Menggunakan loop WHILE:");
-var j = 1;
-while (j <= 10) {
-    if (j % 2 === 0) {
-        console.log(j + " adalah angka genap.");
+var angkaWhile = 1;
+while (angkaWhile <= 20) {
+    if (cekBilanganPrimaWhile(angkaWhile)) {
+        console.log(angkaWhile + " adalah bilangan prima.");
     } else {
-        console.log(j + " adalah angka ganjil.");
+        console.log(angkaWhile + " bukan bilangan prima.");
     }
-    j++;
+    angkaWhile++;
 }
